dc9f720ab1c473c39548841729035d21aeab935b
SurFlurer <22912139+SurFlurer@users.noreply.github.com>
2023-07-25 17:03:35 +0800

    Update full_load_all.sh

--- pyutils/full_load_all.sh
+++ pyutils/full_load_all.sh
@@ -15,7 +15,7 @@ export PATH=$HOME/.local/bin:$PATH
 function run_test
 {
     {
-    set -e pipefail
+    set -eo pipefail
     test_exit_code=0 sed_exit_code=0 exit_code=0
     test_bin=$1
     prefix=$2
